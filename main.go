df85e6ab51238287edfe5a24f69a7d5e56adebec
Elisa Castro de Souza <30930767+Eli15x@users.noreply.github.com>
2023-07-25 21:18:16 -0300

    Feat: init
    Adding main, basic ,with routes, adding client with correct libary (sqlserver) not working yet. Adding Handler and models for User  just structure for now.

---
+++ main.go
@@ -0,0 +1,72 @@
+package main
+
+import (
+	//"time"
+	//"context"
+
+	"context"
+	"fmt"
+	"time"
+
+	"github.com/Eli15x/SagwaBooks-Back/src/client"
+	"github.com/Eli15x/SagwaBooks-Back/src/handlers"
+	"github.com/bugsnag/bugsnag-go"
+	"github.com/gin-contrib/cors"
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+
+	//bugsnag configure
+	bugsnag.Configure(bugsnag.Configuration{
+		APIKey:       "3ecac0ed23b7b1f4b863073135c602b8",
+		ReleaseStage: "production",
+		// The import paths for the Go packages containing your source files
+		ProjectPackages: []string{"main", "github.com/org/myapp"},
+		// more configuration options
+	})
+
+	bugsnag.Notify(fmt.Errorf("Test error"))
+
+	//Context
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+
+	defer cancel()
+	//Connection to Mongo
+	if err := client.GetInstance().Initialize(ctx); err != nil {
+		bugsnag.Notify(fmt.Errorf("[MONGO DB - MovieWorkNow] Could not resolve Data access layer. Error:"))
+	}
+
+	router := gin.Default()
+
+	config := cors.DefaultConfig()
+	config.AllowOrigins = []string{"*"}
+	config.AllowHeaders = []string{"*"}
+	config.AllowMethods = []string{"*"}
+	config.AllowCredentials = true
+
+	router.Use(cors.New(config))
+
+	router.POST("/login", handlers.ValidateUser)
+	router.POST("/cadastro", handlers.CreateUser)
+	router.POST("/user/edit", handlers.EditUser)
+	router.POST("/user/delete", handlers.DeleteUser)
+	router.POST("/user/getByUserId", handlers.DeleteUser)
+	router.POST("/user/getByName", handlers.DeleteUser)
+	router.POST("/user/getByFunction", handlers.DeleteUser)
+	router.POST("/user/getByAcess", handlers.DeleteUser)
+
+	router.POST("/product/create", handlers.CreateWriter)
+	router.GET("/product/getByName", handlers.GetInformationWriter)
+	router.GET("/product/getByCode", handlers.DeleteWriter)
+	router.GET("/product/getByBarCode", handlers.DeleteWriter)
+	router.GET("/product/getAll", handlers.GetInformationWriters)
+	router.POST("/product/edit", handlers.EditWriter)
+	router.DELETE("/product/delete", handlers.DeleteWriter)
+	
+	router.POST("/sale/send", handlers.CreateBook)
+	router.POST("/sales/send", handlers.CreateBook)
+
+
+	router.Run(":1323")
+}
---
+++ src/handler/user.go
@@ -0,0 +1,252 @@
+package handlers
+
+import (
+	"context"
+	"encoding/json"
+
+	"net/http"
+
+	"github.com/Eli15x/ZCOM/src/service"
+	"github.com/gin-gonic/gin"
+	"github.com/labstack/gommon/log"
+)
+
+func ValidateUser(c *gin.Context) {
+	json_map := make(map[string]interface{})
+	err := json.NewDecoder(c.Request.Body).Decode(&json_map)
+
+	if err != nil {
+		c.String(400, "%s", err)
+		return
+	}
+
+	password := json_map["password"].(string)
+	email := json_map["email"].(string)
+
+	if email == "" {
+		c.String(http.StatusBadRequest, "Validate User Error: email not find")
+		return
+	}
+
+	if password == "" {
+		c.String(http.StatusBadRequest, "Create User Error: password not find")
+		return
+	}
+
+	resultUser, err := service.GetInstanceUser().ValidateUser(context.Background(), email, password)
+	if err != nil {
+		c.String(400, err.Error())
+		return
+	}
+
+	c.JSON(http.StatusOK, &resultUser)
+}
+
+func CreateUser(c *gin.Context) {
+
+	json_map := make(map[string]interface{})
+	err := json.NewDecoder(c.Request.Body).Decode(&json_map)
+
+	if err != nil {
+		c.String(400, "%s", err)
+		return
+	}
+
+	name := json_map["name"].(string)
+	email := json_map["email"].(string) 
+	password := json_map["password"].(string)
+	acess := json_map["acess"].(int)
+	userId := json_map["userId"].(string)
+
+	if name == "" {
+		c.String(http.StatusBadRequest, "Create User Error: name not find")
+		return
+	}
+
+	if email == "" {
+		c.String(400, "Create User Error: email not find")
+		return
+	}
+
+	if password == "" {
+		c.String(400, "Create User Error: password not find")
+		return
+	}
+
+	userId, err := service.GetInstanceUser().CreateNewUser(context.Background(), name, email, password, telefone)
+	if err != nil {
+		c.String(400, err.Error())
+		return
+	}
+
+	c.String(http.StatusOK, userId)
+}
+
+func EditUser(c *gin.Context) {
+
+	json_map := make(map[string]interface{})
+	err := json.NewDecoder(c.Request.Body).Decode(&json_map)
+
+	if err != nil {
+		c.String(400, "%s", err)
+		return
+	}
+
+	name := json_map["name"].(string)
+	email := json_map["email"].(string) 
+	password := json_map["password"].(string)
+	acess := json_map["acess"].(int)
+	userId := json_map["userId"].(string)
+
+	if name == "" {
+		c.String(http.StatusBadRequest, "Edit User Error: name not find")
+		return
+	}
+
+	if email == "" {
+		c.String(400, "Edit User Error: email not find")
+		return
+	}
+
+	if password == "" {
+		c.String(400, "Edit User Error: password not find")
+		return
+	}
+
+	if telefone == "" {
+		c.String(400, "Edit User Error: password not find")
+		return
+	}
+
+	if userId == "" {
+		c.String(400, "Edit User Error: password not find")
+		return
+	}
+
+	err = service.GetInstanceUser().EditUser(context.Background(), userId, name, email, password, telefone)
+	if err != nil {
+		c.String(400, err.Error())
+		return
+	}
+
+	c.String(http.StatusOK, "")
+}
+
+func DeleteUser(c *gin.Context) {
+
+	json_map := make(map[string]interface{})
+	err := json.NewDecoder(c.Request.Body).Decode(&json_map)
+
+	if err != nil {
+		c.String(400, "%s", err)
+		return
+	}
+
+	userId := json_map["userId"].(string)
+
+	if userId == "" {
+		c.String(http.StatusBadRequest, "Delete User Error: userId not find")
+		return
+	}
+
+	err = service.GetInstanceUser().DeleteUser(context.Background(), userId)
+	if err != nil {
+		c.String(400, err.Error())
+		return
+	}
+
+	c.String(http.StatusOK, "")
+}
+
+func GetInformationByUserId(c *gin.Context) {
+
+	json_map := make(map[string]interface{})
+	err := json.NewDecoder(c.Request.Body).Decode(&json_map)
+
+	if err != nil {
+		c.String(400, "%s", err)
+		return
+	}
+
+	userId := json_map["userId"].(string) //está dando erro quando tenta pegar o "email" e ele não existe.
+
+	result, err := service.GetInstanceUser().GetInformationUser(context.Background(), userId)
+	if err != nil {
+		c.String(400, err.Error())
+		return
+	}
+
+	log.Infof("[GetInformation] Object : %s \n", result, "")
+
+	c.JSON(http.StatusOK, result)
+}
+
+
+func GetUserByName(c *gin.Context) {
+
+	json_map := make(map[string]interface{})
+	err := json.NewDecoder(c.Request.Body).Decode(&json_map)
+
+	if err != nil {
+		c.String(400, "%s", err)
+		return
+	}
+
+	userId := json_map["userId"].(string) //está dando erro quando tenta pegar o "email" e ele não existe.
+
+	result, err := service.GetInstanceUser().GetInformationUser(context.Background(), userId)
+	if err != nil {
+		c.String(400, err.Error())
+		return
+	}
+
+	log.Infof("[GetInformation] Object : %s \n", result, "")
+
+	c.JSON(http.StatusOK, result) //return list
+}
+
+func GetUserByFunction(c *gin.Context) {
+
+	json_map := make(map[string]interface{})
+	err := json.NewDecoder(c.Request.Body).Decode(&json_map)
+
+	if err != nil {
+		c.String(400, "%s", err)
+		return
+	}
+
+	userId := json_map["userId"].(string) //está dando erro quando tenta pegar o "email" e ele não existe.
+
+	result, err := service.GetInstanceUser().GetInformationUser(context.Background(), userId)
+	if err != nil {
+		c.String(400, err.Error())
+		return
+	}
+
+	log.Infof("[GetInformation] Object : %s \n", result, "")
+
+	c.JSON(http.StatusOK, result) //return list
+}
+
+func GetUserByAcess(c *gin.Context) {
+
+	json_map := make(map[string]interface{})
+	err := json.NewDecoder(c.Request.Body).Decode(&json_map)
+
+	if err != nil {
+		c.String(400, "%s", err)
+		return
+	}
+
+	userId := json_map["userId"].(string) //está dando erro quando tenta pegar o "email" e ele não existe.
+
+	result, err := service.GetInstanceUser().GetInformationUser(context.Background(), userId)
+	if err != nil {
+		c.String(400, err.Error())
+		return
+	}
+
+	log.Infof("[GetInformation] Object : %s \n", result, "")
+
+	c.JSON(http.StatusOK, result) //return list
+}
